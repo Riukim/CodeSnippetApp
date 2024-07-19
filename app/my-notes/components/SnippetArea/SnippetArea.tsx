3dc1ffd726f81c23dd41428f0ccc25e607814ebf
Riukim <m.cominelli29@gmail.com>
2024-07-19 11:04:06 +0200

    Fix layout bug

--- app/my-notes/components/SnippetArea/SnippetArea.tsx
+++ app/my-notes/components/SnippetArea/SnippetArea.tsx
@@ -8,7 +8,13 @@ const SnippetArea = () => {
   } = useAppContext()
 
   return (
-    <div className={`grid max-sm:grid-cols-1 max-lg:grid-cols-2 max-xl:grid-cols-2 gap-4 ${isOpen ? "max-lg:grid-cols-1 max-xl:grid-cols-1 grid-cols-1" : "grid-cols-3"}`}>
+    <div
+      className={`grid gap-4 ${
+        isOpen
+          ? " max-sm:grid-cols-1 max-lg:grid-cols-1 max-xl:grid-cols-1 grid-cols-1"
+          : "grid-cols-3"
+      }`}
+    >
       <SingleSnippet />
       <SingleSnippet />
       <SingleSnippet />
