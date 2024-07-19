04417fb4850074df13928ea803c7d3876092d1e5
Riukim <m.cominelli29@gmail.com>
2024-07-19 11:08:53 +0200

    Fix layout cols layout bug

--- app/my-notes/components/SnippetArea/SnippetArea.tsx
+++ app/my-notes/components/SnippetArea/SnippetArea.tsx
@@ -9,10 +9,10 @@ const SnippetArea = () => {
 
   return (
     <div
-      className={`grid gap-4 ${
+      className={`grid gap-4  ${
         isOpen
           ? " max-sm:grid-cols-1 max-lg:grid-cols-1 max-xl:grid-cols-1 grid-cols-1"
-          : "grid-cols-3"
+          : "grid-cols-3 max-sm:grid-cols-1 max-xl:grid-cols-2"
       }`}
     >
       <SingleSnippet />
