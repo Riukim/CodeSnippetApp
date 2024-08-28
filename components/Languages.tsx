72bc260336026e79bef0b46cb8ab6b8687f78edb
Riukim <m.cominelli29@gmail.com>
2024-08-28 15:17:09 +0200

    Add message to display when no languages are available

--- components/Languages.tsx
+++ components/Languages.tsx
@@ -18,29 +18,38 @@ const Languages = () => {
     snippetsState: { languageCount },
   } = useAppContext()
 
+  const noLanguages = languageCount.length === 0
+
   return (
     <div className="mt-12 text-sm">
       <div className="font-bold">Languages</div>
-      <div className="mt-5 text-slate-400 flex flex-col gap-4">
-        {languageCount.map(({ _id, count }) => (
-          <div
-            key={_id}
-            className="flex justify-between"
-          >
-            <div className="flex gap-2 items-center">
-              <Image
-                src={getIconPath(_id)}
-                alt={`${_id} icon`}
-                width={18}
-                height={18}
-                className="w-5 h-5"
-              />
-              {_id}
+      {noLanguages ? (
+        <p className="mt-5 text-slate-400 w-[80%]">
+          No languages yet.
+          <br /> Add a new snippet to show the languages.
+        </p>
+      ) : (
+        <div className="mt-5 text-slate-400 flex flex-col gap-4">
+          {languageCount.map(({ _id, count }) => (
+            <div
+              key={_id}
+              className="flex justify-between"
+            >
+              <div className="flex gap-2 items-center">
+                <Image
+                  src={getIconPath(_id)}
+                  alt={`${_id} icon`}
+                  width={18}
+                  height={18}
+                  className="w-5 h-5"
+                />
+                {_id}
+              </div>
+              <span className="font-bold">{count}</span>
             </div>
-            <span className="font-bold">{count}</span>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
