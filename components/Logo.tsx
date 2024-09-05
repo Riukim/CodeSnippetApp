1730c85cd83cadb9850795b884c249db4ab84a3b
Riukim <m.cominelli29@gmail.com>
2024-09-05 17:09:21 +0200

    Change logo dimension

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,11 +5,11 @@ const Logo = () => {
     <div className="flex gap-2 items-center">
       <div className="bg-primary p-[6px] rounded-md">
         <Code
-          size={27}
+          size={28}
           color="white"
         />
       </div>
-      <div className="flex gap-1 text-[19px]">
+      <div className="flex gap-1 text-[22px]">
         <span className="font-bold text-primary">Snippet</span>
         <span className="text-foreground">Share</span>
       </div>
